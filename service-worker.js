e45eafd79dba7607c00a8f997f2d121cb7038968
TomKrsck <kraswak@gmail.com>
2019-01-23 19:20:30 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/move-react/precache-manifest.fcc58ebeea97846fc3a79b19945d89d4.js"
+  "/moveReact/precache-manifest.ae42b9139a998a015afc1588e979edc5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/move-react/index.html", {
+workbox.routing.registerNavigationRoute("/moveReact/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
